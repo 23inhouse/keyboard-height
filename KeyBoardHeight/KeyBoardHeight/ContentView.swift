e0e711d7f122bf71eb0dceec9b920ca91566d210
Benjamin Lewis <23inhouse@gmail.com>
2021-10-31 10:04:21 +0100

    Add boilerplate project

---
+++ KeyBoardHeight/KeyBoardHeight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KeyBoardHeight
+//
+//  Created by Benjamin Lewis on 31/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KeyBoardHeight/KeyBoardHeight/KeyBoardHeightApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyBoardHeightApp.swift
+//  KeyBoardHeight
+//
+//  Created by Benjamin Lewis on 31/10/21.
+//
+
+import SwiftUI
+
+@main
+struct KeyBoardHeightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
